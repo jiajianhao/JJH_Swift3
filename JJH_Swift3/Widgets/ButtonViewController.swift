f6de0d31b61130fde452e080027c2e26f7015c37
jiajianhao <macbook@jiadeMacBook-Pro.local>
2016-10-17 16:22:11 +0800

    add new

---
+++ JJH_Swift3/Widgets/ButtonViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ButtonViewController.swift
+//  JJH_Swift3
+//
+//  Created by Jiajianhao on 2016/10/17.
+//  Copyright © 2016年 com.TongHaoDevelopment. All rights reserved.
+//
+
+import UIKit
+
+class ButtonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor=UIColor.white
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
